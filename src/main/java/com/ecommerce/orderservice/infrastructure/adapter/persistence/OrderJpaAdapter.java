e8b44c7be8fc7494f8bd6bd1e496be00627c1a56
Daniel <danielfcgz@gmail.com>
2025-06-18 19:56:06 -0500

    Refactor of search funcionality

--- src/main/java/com/ecommerce/orderservice/infrastructure/adapter/persistence/OrderJpaAdapter.java
+++ src/main/java/com/ecommerce/orderservice/infrastructure/adapter/persistence/OrderJpaAdapter.java
@@ -1,16 +1,12 @@
 package com.ecommerce.orderservice.infrastructure.adapter.persistence;
 
-import com.ecommerce.orderservice.application.exception.InvalidOrderStatusException;
 import com.ecommerce.orderservice.application.mapper.OrderMapper;
 import com.ecommerce.orderservice.domain.gateway.OrderGateway;
 import com.ecommerce.orderservice.domain.model.Order;
-import com.ecommerce.orderservice.domain.model.OrderStatus;
 import com.ecommerce.orderservice.infrastructure.entity.OrderEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -46,31 +42,7 @@ public class OrderJpaAdapter implements OrderGateway {
 
     @Override
     public List<Order> search(String statusStr, Long customerId, String dateStr) {
-        LocalDateTime createdDateTimeStart = null;
-        LocalDateTime createdDateTimeEnd = null;
-        if (dateStr != null && !dateStr.isBlank()) {
-            try {
-                LocalDate date = LocalDate.parse(dateStr);
-                createdDateTimeStart = date.atStartOfDay();
-                createdDateTimeEnd = date.plusDays(1).atStartOfDay();
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Invalid format date.");
-            }
-        }
-
-        OrderStatus status  = null;
-        if (statusStr  != null && !statusStr .isBlank()) {
-            try {
-                status = OrderStatus.valueOf(statusStr.toUpperCase());
-            } catch (IllegalArgumentException ex) {
-                throw new InvalidOrderStatusException(statusStr);
-            }
-        }
-
-
-        List<OrderEntity> entities = orderJpaRepository.searchOrders(status, customerId,
-                createdDateTimeStart, createdDateTimeEnd);
-
+        List<OrderEntity> entities = orderJpaRepository.findAll();
         return entities.stream()
                 .map(OrderMapper::toDomain)
                 .collect(Collectors.toList());
